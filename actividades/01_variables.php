90ffe4b4121fba3d3613fa2f5fd06b89db258e9a
César Melero Valera <cesmelval@alu.edu.gva.es>
2025-10-01 17:02:07 +0200

    esta hecho

---
+++ actividades/01_variables.php
@@ -0,0 +1,12 @@
+
+<?php
+$nombre="zésar";
+$edad=19;
+$edadEn10= $edad+10;
+echo $nombre;
+echo "<br>";
+echo $edad;
+echo "<br>";
+echo $edadEn10;
+echo " hola me llamo $nombre tengo $edad años y en 10 años tendre $edadEn10";
+?>
---
+++ ejemplo/variable.php
@@ -0,0 +1,10 @@
+<?php
+$entero = 4;
+$numero=4.5;
+$cadena="hey";
+$bool=true;
+$agua=5;
+echo gettype($agua);
+echo "<br>";
+$agua="llea";
+?>
