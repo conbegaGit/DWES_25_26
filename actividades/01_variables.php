9fd35076c7ad050feff9b66846e73f6c8216f95c
Ashley Barrionuevo <ashbarzun@alu.edu.gva.es>
2025-10-01 16:58:36 +0200

    01_variables.php

---
+++ actividades/01_variables.php
@@ -0,0 +1,19 @@
+<?php 
+
+/* crea un script en php que declare tres variables:
+una variable $nombre que contenga tu nombre
+una variable $edad que contenga tu edad
+una variable $edadEn10 que almacene tu edad dentro de 10 años
+muestra por pantalla un mensaje que diga:
+"Hola, me llamo [nombre] y tengo [edad] años. Dentro de 10 años tendré [edadEn10] años."
+*/
+
+//variables 
+$nombre = "Ashley"; //nombre
+$edad = 19; //edad actual
+$edadEn10 = $edad + 10;
+
+//muestro los datos por pantalla
+echo "Hola, me llamo $nombre y tengo $edad años. Dentro de 10 años tendré $edadEn10 años.";
+
+?>
