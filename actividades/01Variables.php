c7aceab8233ab6348271805a30ff9d328c2f87fb
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-01 16:53:11 +0200

    Actividades de clase

---
+++ actividades/01Variables.php
@@ -0,0 +1,12 @@
+<?php
+/*Crea un script en PHP que declare tres variables:
+Una variable nombre
+Una variable que almacene la edad que tendras dentro de 10 años
+Tiene que mostrar este mensaje:
+Hola, me llamo [nombre] y dentro de 10 años tendre [edad] años */
+
+$nombre = "Cristian";
+$edad = 20;
+$edadDentroDe10 = $edad + 10;
+echo "Hola, me llamo $nombre y dentro de 10 años tendre $edadDentroDe10 años";
+?>
