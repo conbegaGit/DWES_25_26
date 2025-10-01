efd894504088af2041dbafac6b8df71a340721ec
FranMontesinos <framonsil@alu.edu.gva.es>
2025-10-01 16:55:08 +0200

    Ejemplos y actividad hechas en 1/10/25

---
+++ Actividades/01_variables.php
@@ -0,0 +1,7 @@
+<?php 
+$edad = 29;
+$nombre ="Fran";
+$edadEn10= $edad +10;
+
+echo "Hola, me llamo $nombre, tengo $edad años y dentro de 10 años tendré $edadEn10";
+?>
---
+++ Ejemplo/variablesTiposDeDatos.php
@@ -0,0 +1,11 @@
+<?php
+//Segun el tipo la variable cambia su tipo
+$entero = 17;
+$decimal =1.4;
+$cadena="cadena";
+$booleano=TRUE;
+echo gettype($entero);
+echo "<br>";
+$a ="Prueba";
+echo gettype($a);
+?>
