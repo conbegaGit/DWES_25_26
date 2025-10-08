328d5d9fa2da81cd056007b7697d623d7551ab5a
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-08 16:37:39 +0200

    switch

---
+++ ejemplos/switch.php
@@ -0,0 +1,16 @@
+<?php
+$var = 3;
+switch ($var) {
+    case 1:
+        echo "Es un uno";
+        break;
+    case 2:
+        echo "Es un 2";
+        break;
+    case 3:
+        echo "Es un 3";
+        break;
+    default:
+        echo "var no es un uno ni un dos, ni un tres";
+        break;
+}
