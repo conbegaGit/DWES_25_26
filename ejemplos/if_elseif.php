c5dd6783b6cc5092beb4a64654591618682165ba
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-08 16:32:36 +0200

    nuevo archivio

---
+++ ejemplos/if_elseif.php
@@ -0,0 +1,13 @@
+<?php
+$var = 3;
+if ($var == 1) {
+    echo "var es 1";
+} elseif ($var == 2) {
+    echo "var es 2";
+} 
+elseif ($var == 3) {
+    echo "var es 3";
+}else {
+    echo "var no es un uno ni un dos, ni un tres";
+}
+?>
