179b2a449c56a9d3bd7e51776dd48ba62d254de7
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-08 16:34:37 +0200

    actualisasion

--- ejemplos/if_elseif.php
+++ ejemplos/if_elseif.php
@@ -2,10 +2,9 @@
 $var = 3;
 if ($var == 1) {
     echo "var es 1";
-} elseif ($var == 2) {
+}elseif ($var == 2) {
     echo "var es 2";
-} 
-elseif ($var == 3) {
+}elseif ($var == 3) {
     echo "var es 3";
 }else {
     echo "var no es un uno ni un dos, ni un tres";
