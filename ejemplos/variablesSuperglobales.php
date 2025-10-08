55016b5a1effc7a19d94659af5c1667fdd6fbabc
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-08 15:53:55 +0200

    nuevo archivo

---
+++ ejemplos/variablesSuperglobales.php
@@ -0,0 +1,6 @@
+<?php
+echo "Ruta dentro de htdocs: " . $_SERVER['PHP_SELF'] . "<br>";
+echo "Nombre del servidor: " . $_SERVER['SERVER_NAME'] . "<br>";
+echo "Software del servidor: " . $_SERVER['SERVER_SOFTWARE'] . "<br>";
+echo "Protocolo: " . $_SERVER['SERVER_PROTOCOL'] . "<br>";
+echo "Metodo de envio: " . $_SERVER['REQUEST_METHOD'];
