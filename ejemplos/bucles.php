7b241fa5e8ea9341587b0d51fb00ab50f3d78e91
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-08 16:47:20 +0200

    actualizacion

--- ejemplos/bucles.php
+++ ejemplos/bucles.php
@@ -3,10 +3,12 @@ for ($i=0; $i < 10; $i++) {
     echo $i . "<br>";
 }
 echo "<br>";
+$i = 0;
 do {
     echo $i . " <br>";
     $i++;
 } while ($i < 10);
+$i = 0;
 echo "<br>";
 while ($i < 10) {
     echo $i . " <br>";
