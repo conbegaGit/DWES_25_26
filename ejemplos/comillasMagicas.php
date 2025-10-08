1885731e3a3deae5ae585c6692fe4983bd5a7a5c
Cristian Krasimirov Nikolov <crikranik@alu.edu.gva.es>
2025-10-08 16:18:46 +0200

    nuevos archivos

---
+++ ejemplos/comillasMagicas.php
@@ -0,0 +1,11 @@
+<?php
+$var = "Paco";
+echo "hola $var <br>"; //hola Paco
+echo 'hola $var <br>'; //hola $var
+echo "Hola " . $var; //hola Paco
+$nombre = "Pau";
+$apellido = "Marti";
+echo $nombre . " " . $apellido; //Pau Marti
+$a = "Modulo ";
+$b = $a . "DWES";
+$a .= "DWES";
