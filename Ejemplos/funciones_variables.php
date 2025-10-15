b5f0308771212bc31a6f924b9bcf6a1468511e0f
Miguel Duque Perez <migduqper@alu.edu.gva.es>
2025-10-15 16:50:51 +0200

    jeje el que faltaba

---
+++ Ejemplos/funciones_variables.php
@@ -0,0 +1,25 @@
+<?php
+    $var1 = 4;
+    $var2 = NULL;
+    $var3 = FALSE;
+    $var4 = 0;
+    echo "var1 <br>";
+    var_dump(isset($var1));
+    var_dump(is_null($var1));
+    var_dump(empty($var1));
+
+
+    echo "<br> var 2 <br>";
+    var_dump(isset($var2));
+    var_dump(is_null($var2));
+    var_dump(empty($var2));
+
+    echo "<br> var 3 <br>";
+    var_dump(isset($var3));
+    var_dump(is_null($var3));
+    var_dump(empty($var3));
+
+    echo "<br> var 4 <br>";
+    var_dump(isset($var4));
+    var_dump(is_null($var4));
+    var_dump(empty($var4));
