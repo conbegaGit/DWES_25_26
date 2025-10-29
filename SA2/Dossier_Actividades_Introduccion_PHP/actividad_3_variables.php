e2f7f5ffdd20f37b525ec703301b5aaf8f971ddf
davcruman <davcruman@alu.edu.gva.es>
2025-10-29 16:43:08 +0100

    dossier

--- SA2/Dossier_Actividades_Introduccion_PHP/actividad_3_variables.php
+++ SA2/Dossier_Actividades_Introduccion_PHP/actividad_3_variables.php
@@ -1,15 +1,23 @@
 <?php
-$nombre = "Juan"; 
-$edad = 0; 
-$saldo; // No definida 
+
+$nombre = "Juan";
+$edad = 0;
 $email = null;
-$lista = array();
+$lista = [];
 
+$variables = ["nombre", "edad", "saldo", "email", "lista"];
 
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Variable</th><th>isset()</th><th>empty()</th><th>is_null()</th></tr>";
 
-
-
-
-
+foreach ($variables as $var) {
+    echo "<tr>";
+    echo "<td>\$$var</td>";
+    echo "<td>" . (isset($$var) ? 'true' : 'false') . "</td>";
+    echo "<td>" . (empty($$var) ? 'true' : 'false') . "</td>";
+    echo "<td>" . (is_null($$var) ? 'true' : 'false') . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
 
 
