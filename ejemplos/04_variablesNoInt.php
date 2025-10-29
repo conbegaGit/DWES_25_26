ba86b3eb8705f62651bdf4ffbf4de5cca17dced3
Ángel Sáez Díaz <angsaedia@alu.edu.gva.es>
2025-10-29 16:59:51 +0100

    Organizacion

--- ejemplos/04_variablesNoInt.php
+++
@@ -1,7 +0,0 @@
-<?php
-$var1 = 100;
-$var3 = 100 + $var2; // $var2 no existe, asi que toma valor 0.
-echo "$var3 <br>"; // Muestra 100
-$var3 = 100 * $var2; // $var2 no existe, asi que toma valor 0.
-echo "$var3 <br>"; // Muestra 0
-?>
--- ejemplos/11_requerir.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $a = "variable a del principal";
-    require "12_ejerequerido.php";
-    $b = "otra variable b del principal";
-    echo "4.  En el script principal <br>";
