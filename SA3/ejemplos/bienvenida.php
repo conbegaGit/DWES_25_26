5e97e5d88ef375773716d87d1fa2750433a4229e
davcruman <davcruman@alu.edu.gva.es>
2025-11-05 16:44:26 +0100

    index, bienvenida

---
+++ SA3/ejemplos/bienvenida.php
@@ -0,0 +1,28 @@
+<?php
+if (isset($_POST['nombre'])&& !empty($_POST['nombre']) && isset($_POST['color']) && !empty($_POST['color'])) {
+    $nombre = $_POST['nombre'];
+    $color = $_POST['color'];
+    setcookie("nombre", $nombre, time() + 3600); 
+    setcookie("color", $color, time() + 3600);
+} elseif (isset($_COOKIE["nombre"]) && isset($_COOKIE["color"])) {
+    $color = $_COOKIE["color"]; 
+    $nombre = $_COOKIE["usuario"];
+} else {
+    header ("Location: index.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset = "UTF-8">
+    <title>Página de Bienvenida</title>
+</head>
+<body style= "background-color: <?php echo htmlspecialchars($color); ?>;">
+    <h1>Hola, <?php echo htmlspecialchars($nombre); ?>!</h1>
+    <p>Encantad@ de verte de nuevo</p>
+    <p><a href="borrar_cookie.php">Cerrar sesión</a><p>
+</body>
+</html>
---
+++ SA3/ejemplos/borrar_cookie.php
@@ -0,0 +1,15 @@
+<?php
+setcookie("usuario", "", time() - 3600);
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset = "UTF-8">
+    <title>Cooki borrada</title>
+</head>
+
+<body>
+    <h1>Has ccerrado sesión correctamente</h1>
+    <a href="index.php">Volver al inicio</a>
