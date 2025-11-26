753eddb8e53269c954580ac65e632880112c1114
Ashley Barrionuevo <ashbarzun@alu.edu.gva.es>
2025-11-26 17:41:35 +0100

    26/11

--- SA3/cookies_sesiones/login.php
+++ SA3/cookies_sesiones/login.php
@@ -1,12 +1,34 @@
 <?php
 
+//TODAVIA NO ESTA COMPLETOOOO :(
+ $cadena_conexion = 'mysql:dbname=empresa;host=localhost;charset=utf8';
+$usuario = 'root';
+$clave = '';  
+
 session_start();
 
-$usuario = $_POST["usuario"] ?? '';
-$clave = $_POST ["clave"] ?? '';
+$usuario1 = $_POST['usuario'] ?? '';
+$clave = $_POST['clave'] ?? '';
 
 
-if ($usuario === "Ashley" && $clave === "1234") {
+        try{
+            $bd = new PDO($cadena_conexion, $usuario, $clave);
+            $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            // Le dice a PHP que capture el error en el chatch 
+
+
+        $consulta = $bd->query("SELECT * FROM usuarios");
+            $buscar = false;
+            foreach($consulta as $fila){
+                if ($usuario1 === $fila['Nombre']  && $clave === $fila['Clave']) {
+                    $buscar = true;
+                }
+            }   
+    } catch (PDOException $e){
+        echo "Error de conexión: " . $e->getMessage();
+    }
+
+if ($buscar == true) {
     $_SESSION['usuario'] = $usuario;
     if (isset($_POST['recordar'])) {
         setcookie('usuariorec', $usuario, time() + (3600));
