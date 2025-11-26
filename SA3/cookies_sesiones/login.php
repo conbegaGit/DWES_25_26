dbe506c90a1422902aa8070127316b644763a5d7
Juan José Bedoya Isaza <juabedisa@alu.edu.gva.es>
2025-11-26 17:04:00 +0100

    Avances de ejercicios y base de datos del día 26/11/2025

--- SA3/cookies_sesiones/login.php
+++ SA3/cookies_sesiones/login.php
@@ -3,20 +3,41 @@
 
     $usuario = $_POST["usuario"];
     $clave = $_POST["clave"];
+    $cadena_conexion = 'mysql:dbname=empresa;host=127.0.0.1';
+    $usuarioBD = 'root';
+    $clave = '';
+    try{
+        $db = new PDO($cadena_conexion, $usuario, $clave);
+        $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $encontrado = null;
+        $consulta = $bd->query("SELECT Nombre FROM usuarios");
+        $usuario = $_POST["usuario"];
+        $clave = $_POST["clave"];
 
-    if($usuario === "admin" && $clave === "1234"){
-        $_SESSION['usuario'] = $usuario;
-        $_SESSION['clave'] = $clave;
-        
-        if(isset($_POST['recordar'])){
-            setcookie("cookie_usuario", $usuario, time() + (7 * 24 * 60 * 60));
+        foreach($consulta as $dato){
+            if($dato == $usuario){
+                $encontrado = true;
+                break;
+            }
         }
 
-        header("Location: bienvenida.php");
-        exit;
-    }
+        if($usuario === "admin" && $clave === "1234"){
+            $_SESSION['usuario'] = $usuario;
+            $_SESSION['clave'] = $clave;
+        
+            if(isset($_POST['recordar'])){
+                setcookie("cookie_usuario", $usuario, time() + (7 * 24 * 60 * 60));
+            }
+
+            header("Location: bienvenida.php");
+            exit;
+        }
     
-    else{
-        echo "<p>Usuario o contraseña incorrecto</p>";
-        echo '<a href="index.php">Volver</a>';
+    
+        else{
+            echo "<p>Usuario o contraseña incorrecto</p>";
+            echo '<a href="index.php">Volver</a>';
+        }
+    }catch(PDOException $e){
+        echo 'Error con la base de datos: ' . $e->getMessage();
     }
---
+++ SA4/Actividades/bd_empresa.php
@@ -0,0 +1,71 @@
+<?php
+    $cadena_conexion = 'mysql:dbname=empresa;host=127.0.0.1';
+    $usuario = 'root';
+    $clave = '';
+
+    try{
+        $bd = new PDO($cadena_conexion, $usuario, $clave);
+        $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        /*Le dice a PHP que:
+        1. Si ocurre un error en la base de datos,
+        2. PDO NO debe devolver un false silecioso,
+        3. Sino lanzar una excepción (PDOException).
+        Si NO lo ponemos, un error como una consulta mal escrita o una tabla que no existe
+        simplemente devuelve false y no te dice por qué ha fallado, lo que dificulta
+        mucho depurar.
+        */
+
+        echo "<h3>Conexión a la base de datos con éxito :)</h3>";
+        $consulta = $bd->query("SELECT * FROM empleados");
+
+        echo "<h4>Empleados: </h4>";
+        echo "<table border='1', cellpadding='5'>";
+        echo "<tr><th>CODEMP</th><th>Nombre</th><th>Apellido</th><th>Departamento</th></tr>";
+            
+        foreach($consulta as $fila){
+            echo "<tr>
+                <td>{$fila['CodEmple']}</td>
+                <td>{$fila['Nombre']}</td>
+                <td>{$fila['Apellido1']}</td>
+                <td>{$fila['Departamento']}</td>
+                </tr>";
+        }
+        echo "</table>";
+
+        echo '<br>';
+        $consulta = $bd->query("SELECT * FROM departamentos");
+        echo "<h4>Departamentos: </h4>";
+        echo "<table border='1', cellpadding='5'>";
+        echo "<tr><th>CODDEPT</th><th>Nombre</th><th>Jefe</th><th>Presupuesto</th><th>Ciudad</th></tr>";
+            
+        foreach($consulta as $fila){
+            echo "<tr>
+                <td>{$fila['CodDept']}</td>
+                <td>{$fila['Nombre']}</td>
+                <td>{$fila['Jefe']}</td>
+                <td>{$fila['Presupuesto']}</td>
+                <td>{$fila['Ciudad']}</td>
+                </tr>";
+        }
+        echo "</table>";
+
+        echo '<br>';
+        $consulta = $bd->query("SELECT * FROM usuarios");
+        echo "<h4>Usuarios: </h4>";
+        echo "<table border='1', cellpadding='5'>";
+        echo "<tr><th>Codigo</th><th>Nombre</th><th>Clave</th><th>Rol</th></tr>";
+            
+        foreach($consulta as $fila){
+            echo "<tr>
+                <td>{$fila['Codigo']}</td>
+                <td>{$fila['Nombre']}</td>
+                <td>{$fila['Clave']}</td>
+                <td>{$fila['Rol']}</td>
+                </tr>";
+        }
+        echo "</table>";
+
+    }catch(PDOException $e){
+        echo 'Error con la base de datos: ' . $e->getMessage();
+    }
+?>
