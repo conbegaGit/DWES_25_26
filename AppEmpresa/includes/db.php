04858fb2a1acfce980b99d75ffd2a25f3739cd6e
davcruman <davcruman@alu.edu.gva.es>
2025-12-03 16:54:30 +0100

    carpetas

---
+++ AppEmpresa/includes/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "127.0.0.1";      
+$dbname = "empresa";      
+$pass = "";              
+$user = "root";              
+
+try {
+    $bd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $bd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    die("Error en la conexión: " . $e->getMessage());
+}
+?>
