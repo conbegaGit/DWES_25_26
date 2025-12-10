257fd8236b9d8d48328c1b82565f13f477b78e74
David Caballero Burgos <davcabbur@alu.ed.gva.es>
2025-12-10 16:09:01 +0100

    Update

--- AppEmpresa/dashboard.php
+++ AppEmpresa/dashboard.php
@@ -1,20 +1,19 @@
 <?php
-session_start();
-require_once "include/auth.php";
-require_once "includes/db.php";
-require_once "includes/functions.php";
-require_once "includes/header.php";
+    session_start();
+    //require_once "includes/auth.php";
+    require_once "includes/db.php";
+    require_once "includes/functions.php";
+    require_once "includes/header.php";
 
-$tot_dept = $db->query("SELECT COUNT(*) FROM departamentos")->fetchColumn();
-$tot_emp = $db->query("SELECT COUNT(*) FROM empleados")->fetchColumn();
-$tot_user = $db->query("SELECT COUNT(*) FROM usuarios")->fetchColumn();
+    $tot_dept = $bd->query("SELECT COUNT(*) FROM departamentos")->fetchColumn();
+    $tot_emp = $bd->query("SELECT COUNT(*) FROM empleados")->fetchColumn();;
+    $tot_user = $bd->query("SELECT COUNT(*) FROM usuarios")->fetchColumn();;
 ?>
 <h2>Panel principal</h2>
-
 <div class="grid">
-    <div class="card">Departamentos <span class="big"><?= e($tot_dept) ?></span></div>
-    <div class="card">Empleados <span class="big"><?= e($tot_emp) ?></span></div>
-    <div class="card">Usuarios <span class="big"><?= e($tot_user) ?></span></div>
+    <div class="card">Departamentos<span class="big"><?= e($tot_dept) ?></div>
+    <div class="card">Empleados<span class="big"><?= e($tot_dept) ?></div>
+    <div class="card">Usuarios<span class="big"><?= e($tot_dept) ?></div>
 </div>
 
 <?php require_once "includes/footer.php"; ?>
