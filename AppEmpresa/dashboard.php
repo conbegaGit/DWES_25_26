438f55d5b8f2d01af3a36dd3b2555b2be4430b01
César Melero Valera <cesmelval@alu.edu.gva.es>
2025-12-10 17:03:57 +0100

    vale ya tengo elk inicioooo

--- AppEmpresa/dashboard.php
+++ AppEmpresa/dashboard.php
@@ -5,10 +5,11 @@ require_once "includes/db.php";
 require_once "includes/functions.php";
 require_once "includes/header.php";
 
-$tot_dept = $db->query("SELECT COUNT(*)FROM departamentos")->fetchColumn();
-$tot_emp =$db->query("SELECT COUNT (*) FROM empleados")->fetchColumn();
-$tot_user = $db->query("SELECT COUNT (*) FROM usuarios")->fetchColumn();
+$tot_dept = $db->query("SELECT COUNT(*) FROM departamentos")->fetchColumn();
+$tot_emp = $db->query("SELECT COUNT(*) FROM empleados")->fetchColumn();
+$tot_user = $db->query("SELECT COUNT(*) FROM usuarios")->fetchColumn();
 ?>
+
 <h2>Panel principal</h2>
 
 <div class="grid">
@@ -16,4 +17,6 @@ $tot_user = $db->query("SELECT COUNT (*) FROM usuarios")->fetchColumn();
     <div class="card">Empleados <span class="big"><?= e($tot_emp) ?></span></div>
     <div class="card">Usuarios <span class="big"><?= e($tot_user) ?></span></div>
 </div>
-<?php require_once "includes/footer.php";?>
+
+<?php require_once "includes/footer.php" ?>
+
