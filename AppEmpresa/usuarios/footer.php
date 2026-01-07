e31c4b5079b0aa7688751cd56badc4bfadbdf096
davcruman <davcruman@alu.edu.gva.es>
2026-01-07 15:21:10 +0100

    commit

---
+++ AppEmpresa/usuarios/footer.php
@@ -0,0 +1,10 @@
+<?php
+?>
+</main>
+    <footer class="site-footer">
+        <div class="wrap">
+            <small>&copy; <?= date("Y"); ?> Empresa - Proyecto 2DAM</small>
+        </div>
+    </footer>
+    </body>
+</html>
