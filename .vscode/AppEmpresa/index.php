e44c74c8430148fded543129e25cb60d23a67cac
Marcelo Moreira Pereira <marmormai@alu.edu.gva.es>
2026-01-07 16:34:26 +0100

    ..

--- .vscode/AppEmpresa/index.php
+++ .vscode/AppEmpresa/index.php
@@ -1,4 +1,3 @@
-
 <?php
 include "../AppEmpresa/includes/db.php";
 session_start();
@@ -10,7 +9,7 @@ session_start();
     <meta charset="utf-8">
     <title>Login - Empresa</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="/AppEmpresa/css/style.css">
+    <link rel="stylesheet" href="../AppEmpresa/css/style.css">
 </head>
 <body class="login-body">
 <div class="login-box">     
