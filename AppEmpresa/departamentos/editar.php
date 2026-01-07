4e714b8c1e88860c32210f2768990de0aada375d
César Melero Valera <cesmelval@alu.edu.gva.es>
2026-01-07 14:48:44 +0100

    2 comits

---
+++ AppEmpresa/departamentos/editar.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+//require_once "includes/auth.php";
+require_once "../includes/db.php";
+require_once "../includes/functions.php";
+
+$id = intval($_GET['id'] ?? 0);
+$stmt = $db->prepare("SELECT * FROM DEPARTAMENTOS WHERE CodDept = ?");
+$stmt->execute([$id]);
+$dept = $stmt->fetch(PDO::FETCH_ASSOC);
+IF (!$dept){
+    //flash_set("departamento no encontrado");
+    exit;
+}
+
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $nombre = trim($_POST['Nombre']);
+    $ciudad = trim($_POST['Ciudad']);
+    $presupuesto= intval($_POST['Presupuesto']);
+    $jefe =!empty($_POST['Jefe']) ? intval($_POST['Jefe']) : null;
+    $db->prepare("UPDATE departamentos SET Nombre=?,Presupuesto=?, Jefe? WHERE CodDept")
+       ->execute([$nombre, $ciudad, $presupuesto, $jefe, $id]);
+       //flash_set("Departamento actualizado");
+       header("Location: listar.php");
+       exit; 
+}
+
+require_once "../includes/header.php";
+?>
+<h2>Editar Departamento</h2>
+<form method="post">
+    <label>Nombre<br><input type="text" name="Nombre" value="<?= e($dept ['Nombre']) ?>"required></label>
+    <label>Ciudad<br><input type="text" name="Ciudad" value="<?= e($dept ['Ciudad']) ?>"required></label>
+    <label>Presupuesto<br><input type="text" name="Presupuesto" value="<?= e($dept ['Presupuesto']) ?>"required></label>
+    <label>Jefe<br>
+        <select name="Jefe">
+            <option value="">-- Ninguno --</option>
+            <?php foreach ($emps as $em): ?>
+                <option value="<? $em['CodEmple']?>"
+                <?= ($dept['Jefe']==$em['CodEmple'])? 'selected':'' ?>>
+                <?=  e($em['Nombre'].''.$em['Apellido1']) ?>
+                </option>
+                <?php endforeach; ?>
+        </select>
+
+    </label>
+    <div class="actions"><button type="submit">Guardar</button><a class="btn" href="listar.php">Cancelar</a></div>
+</form>
+<?php require_once "../includes/footer.php"; ?>
